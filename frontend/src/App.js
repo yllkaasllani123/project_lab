18d7501b382450c402c11bfa98270beef7a9da6d
yllkaasllani123 <151162713+yllkaasllani123@users.noreply.github.com>
2024-05-13 01:06:45 +0200

    ndryshime ne app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HotelManagement from './pages/HotelManagement';
+import PrivateRoutes from './PrivateRoutes';
+import {theme} from "./themes/default";
+import {ThemeProvider} from "@mui/material";
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+          <Route element={<PrivateRoutes />}>
+            <Route element={<HotelManagement/>} path="/hotel-management"/>
+          </Route>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
